decbde453ee2aca8ae0bc6209d1c5fd7fe2ed061
Moeez Tahir <42956596+moeeztaher@users.noreply.github.com>
2024-09-15 23:03:46 +0200

    commit: frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  externals: {
+    JIRA: 'JIRA', // This ensures the JIRA global variable is recognized
+  }
+};
